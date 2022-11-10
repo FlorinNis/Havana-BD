61ec2e7830f241efd972de759feb1e899f6d0305
florinnis3 <florin.nistor@student.tuiasi.ro>
2022-11-10 11:19:25 +0200

    Interface

---
+++ Interface/Window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+
+def startWindow():
+    window.title("Havana Bar")
+    window.configure(width=500, height=500)
+    window.configure(bg='lightgrey')
+
+    window.mainloop()
---
+++ main.py
@@ -0,0 +1,4 @@
+from Interface.Window import *
+
+startWindow()
+
