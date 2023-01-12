2930a5fc02c3b9a72862349854866a4a68a54ea2
FlorinNis <florin.nistor@student.tuiasi.ro>
2023-01-12 16:24:22 +0200

    Proiect final

--- Interface/WindowProduse.py
+++ Interface/WindowProduse.py
@@ -1,16 +1,19 @@
+import sqlite3
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import BazaDate.baza as baza
 
-id=1
+id=baza.c.execute('''SELECT id FROM produse ORDER BY id DESC LIMIT 1''').fetchone()[0] + 1
+if id is None:
+    id=1
 
 def startNewWindow(newWindow):
     newWindow.iconify()
     newWindow.deiconify()
 
     newWindow.title("Havana Bar")
-    newWindow.geometry("500x600")
+    newWindow.geometry("500x500")
     #window.configure(width=500, height=500)
     newWindow.configure(bg='lightgrey')
 
@@ -83,8 +86,8 @@ def tableProduse(newWindow):
     input_frame=Frame(newWindow)
     input_frame.pack()
 
-    id = Label(input_frame,text="ID")
-    id.grid(row=0,column=0)
+    #id = Label(input_frame,text="ID")
+    #id.grid(row=0,column=0)
 
     produs_nume = Label(input_frame,text="Nume produs")
     produs_nume.grid(row=0,column=1)
@@ -95,8 +98,8 @@ def tableProduse(newWindow):
     produs_stock = Label(input_frame, text="Stock Produs")
     produs_stock.grid(row=0, column=3)
 
-    id_entry = Entry(input_frame)
-    id_entry.grid(row=1,column=0)
+    #id_entry = Entry(input_frame)
+    #id_entry.grid(row=1,column=0)
 
     produs_nume_entry = Entry(input_frame)
     produs_nume_entry.grid(row=1,column=1)
@@ -109,18 +112,19 @@ def tableProduse(newWindow):
 
 
     def input_record():
-        global count
+        global count, id
 
-        if id_entry.get() and produs_nume_entry.get():
-            my_table.insert(parent='', index='end',iid=count, text='', values =(id_entry.get(), produs_nume_entry.get(), produs_pret_entry.get(), produs_stock_entry.get()))
+        if produs_nume_entry.get():
+            my_table.insert(parent='', index='end',iid=count, text='', values =(id, produs_nume_entry.get(), produs_pret_entry.get(), produs_stock_entry.get()))
             count += 1
 
-            baza.c.execute('''INSERT INTO produse (id, nume) values(?,?)''', (id_entry.get(),produs_nume_entry.get()))
-            baza.c.execute('''INSERT INTO produse_detalii (produs_id, pret, stock) values(?,?,?)''', (id_entry.get(), produs_pret_entry.get(), produs_stock_entry.get()))
+
+            baza.c.execute('''INSERT INTO produse (id, nume) values(?,?)''', (id,produs_nume_entry.get()))
+            baza.c.execute('''INSERT INTO produse_detalii (produs_id, pret, stock) values(?,?,?)''', (id, produs_pret_entry.get(), produs_stock_entry.get()))
             baza.conn.commit()
 
-
-            id_entry.delete(0,END)
+            id += 1
+            #id_entry.delete(0,END)
             produs_nume_entry.delete(0,END)
             produs_pret_entry.delete(0,END)
             produs_stock_entry.delete(0,END)
@@ -136,31 +140,35 @@ def tableProduse(newWindow):
 
     #Buton
     input_button = Button(newWindow, text="Adaugare Produs",command=input_record)
-    input_button.pack()
+    input_button.place(x=100,y=300)
 
     #Select Record
     def select_record():
-        id_entry.delete(0, END)
+        global idT
+        #id_entry.delete(0, END)
         produs_nume_entry.delete(0, END)
         produs_pret_entry.delete(0, END)
         produs_stock_entry.delete(0, END)
 
         selected = my_table.focus()
         values = my_table.item(selected,'values')
-        id_entry.insert(0,values[0])
+        #id_entry.insert(0,values[0])
         produs_nume_entry.insert(0,values[1])
         produs_pret_entry.insert(0,values[2])
         produs_stock_entry.insert(0,values[3])
+        idT = baza.c.execute('''SELECT id FROM produse WHERE nume = ?''', (produs_nume_entry.get(),)).fetchone()[0]
+
 
     def actualizare_nume():
-        if id_entry.get() and produs_nume_entry.get():
+        if produs_nume_entry.get():
             selected = my_table.focus()
-            my_table.item(selected,text="",values=(id_entry.get(), produs_nume_entry.get(), produs_pret_entry.get(),produs_stock_entry.get()))
 
-            baza.c.execute('''UPDATE produse SET nume = ? WHERE id = ?''', (produs_nume_entry.get(), id_entry.get()))
+            my_table.item(selected,text="",values=(idT, produs_nume_entry.get(), produs_pret_entry.get(),produs_stock_entry.get()))
+
+            baza.c.execute('''UPDATE produse SET nume = ? WHERE id = ?''', (produs_nume_entry.get(), idT))
             baza.conn.commit()
 
-            id_entry.delete(0, END)
+            #id_entry.delete(0, END)
             produs_nume_entry.delete(0, END)
             produs_pret_entry.delete(0, END)
             produs_stock_entry.delete(0, END)
@@ -175,14 +183,15 @@ def tableProduse(newWindow):
             root.mainloop()
 
     def actualizare_pret():
-        if id_entry.get() and produs_pret_entry.get():
+        if produs_pret_entry.get():
             selected = my_table.focus()
-            my_table.item(selected,text="",values=(id_entry.get(), produs_nume_entry.get(), produs_pret_entry.get(),produs_stock_entry.get()))
 
-            baza.c.execute('''UPDATE produse_detalii SET pret = ? WHERE produs_id = ?''', (produs_pret_entry.get(), id_entry.get()))
+            my_table.item(selected,text="",values=(idT, produs_nume_entry.get(), produs_pret_entry.get(),produs_stock_entry.get()))
+
+            baza.c.execute('''UPDATE produse_detalii SET pret = ? WHERE produs_id = ?''', (produs_pret_entry.get(), idT))
             baza.conn.commit()
 
-            id_entry.delete(0, END)
+            #id_entry.delete(0, END)
             produs_nume_entry.delete(0, END)
             produs_pret_entry.delete(0, END)
             produs_stock_entry.delete(0, END)
@@ -199,14 +208,29 @@ def tableProduse(newWindow):
     def actualizareStock():
         if produs_stock_entry.get():
             selected = my_table.focus()
-            my_table.item(selected, text="", values=(id_entry.get(), produs_nume_entry.get(), produs_pret_entry.get(), produs_stock_entry.get()))
 
-            stock_produse_detalii = baza.c.execute('''SELECT stock FROM produse_detalii WHERE produs_id = ?''', (id_entry.get(),))
+            my_table.item(selected, text="", values=(idT, produs_nume_entry.get(), produs_pret_entry.get(), produs_stock_entry.get()))
+
+            stock_produse_detalii = baza.c.execute('''SELECT stock FROM produse_detalii WHERE produs_id = ?''', (idT,))
             stock = stock_produse_detalii.fetchone()[0]
-            baza.c.execute('''insert into stock_final (Stock_id, Stock_Initial, Stock_Final) values(?,?,?)''', (id_entry.get(), stock, produs_stock_entry.get()))
+            baza.c.execute('''insert into stock_final (Stock_id, Stock_Initial, Stock_Final) values(?,?,?)''', (idT, stock, produs_stock_entry.get()))
             baza.conn.commit()
 
-            id_entry.delete(0, END)
+            try:
+                baza.c.execute('''
+                                        INSERT INTO raport_final_zi (nume_produs, stock_vandut, profit)
+                                        SELECT p.nume, (sf.Stock_Initial - sf.Stock_Final), (sf.Stock_Initial - sf.Stock_Final) * pd.pret
+                                        FROM produse p
+                                        INNER JOIN stock_final sf ON p.id = sf.Stock_id
+                                        INNER JOIN produse_detalii pd ON p.id = pd.produs_id
+                                        WHERE p.id = ?''', (idT,))
+            except sqlite3.Error as e:
+                print(e)
+            baza.conn.commit()
+            print("Raportul a fost generat cu succes!")
+            print(idT)
+
+            #id_entry.delete(0, END)
             produs_nume_entry.delete(0, END)
             produs_pret_entry.delete(0, END)
             produs_stock_entry.delete(0, END)
@@ -214,30 +238,23 @@ def tableProduse(newWindow):
     def actualizareStockMarfaNoua():
         if produs_stock_entry.get():
             selected = my_table.focus()
-            my_table.item(selected, text="", values=(id_entry.get(), produs_nume_entry.get(), produs_pret_entry.get(), produs_stock_entry.get()))
 
-            baza.c.execute('''UPDATE produse_detalii SET stock = ? WHERE produs_id = ?''', (produs_stock_entry.get(), id_entry.get()))
+            my_table.item(selected, text="", values=(idT, produs_nume_entry.get(), produs_pret_entry.get(), produs_stock_entry.get()))
+
+            baza.c.execute('''UPDATE produse_detalii SET stock = ? WHERE produs_id = ?''', (produs_stock_entry.get(), idT))
             baza.conn.commit()
 
-            id_entry.delete(0, END)
+            #id_entry.delete(0, END)
             produs_nume_entry.delete(0, END)
             produs_pret_entry.delete(0, END)
             produs_stock_entry.delete(0, END)
 
     def raportFinalZi():
 
-        #Adaugare in tabelul raport_final_zi
-        baza.c.execute('''
-                        INSERT INTO raport_final_zi (nume_produs, stock_vandut, profit)
-                        SELECT p.nume, (sf.Stock_Initial - sf.Stock_Final), (sf.Stock_Initial - sf.Stock_Final) * pd.pret
-                        FROM produse p
-                        INNER JOIN stock_final sf ON p.id = sf.Stock_id
-                        INNER JOIN produse_detalii pd ON p.id = pd.produs_id
-                    ''')
-        baza.conn.commit()
-
         Total = baza.c.execute('''SELECT SUM(profit) FROM raport_final_zi''')
         total = Total.fetchone()[0]
+
+
 
 
         raport = Tk()
@@ -247,6 +264,15 @@ def tableProduse(newWindow):
 
         raport_frame = Frame(raport)
         raport_frame.pack()
+
+        string_var = StringVar()
+        string_var.set(total)
+        totalLabel = Label(raport, textvariable=string_var, font=("Helvetica", 16), bg='lightgrey')
+        totalLabel.place(x=370, y=300)
+        TotalLab= Label(raport, text="Total:", font=("Helvetica", 16), bg='lightgrey')
+        TotalLab.place(x=300, y=300)
+        leiLabel = Label(raport, text="lei", font=("Helvetica", 16), bg='lightgrey')
+        leiLabel.place(x=450, y=300)
 
         # scrollbar
         table_scroll = Scrollbar(raport_frame)
@@ -292,22 +318,22 @@ def tableProduse(newWindow):
     #Butoane
 
     select_button = Button(newWindow, text="Selectare Produs", command=select_record)
-    select_button.pack(pady=10)
+    select_button.place(x=310,y=300)
 
     nume_button = Button(newWindow, text="Actualizare Nume",command=actualizare_nume)
-    nume_button.pack(pady=10)
+    nume_button.place(x=100,y=350)
 
     pret_button = Button(newWindow, text="Actualizare Pret",command=actualizare_pret)
-    pret_button.pack(pady=10)
+    pret_button.place(x=310,y=350)
 
     stock_button = Button(newWindow, text="Actualizare Stock Final Zi", command=actualizareStock)
-    stock_button.pack(pady=10)
+    stock_button.place(x=80,y=400)
 
     stockPlus_button = Button(newWindow, text="Actualizare Stock Marfa Noua", command=actualizareStockMarfaNoua)
-    stockPlus_button.pack(pady=10)
+    stockPlus_button.place(x=280,y=400)
 
     raportFinal = Button(newWindow, text="Raport Final Zi", command=raportFinalZi)
-    raportFinal.pack(pady=10)
+    raportFinal.place(x=200,y=450)
 
 
 
